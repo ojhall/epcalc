bcf5d3ed3a077af2331f3cc13343382badff7652
Owen Hall <owen.hall@red-gate.com>
2020-04-07 15:00:30 +0100

    Add NewApp.svelte.
    Untrack generated files.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,4 @@
-import App from './App.svelte';
-
-// const app = new App({
-// 	target: document.body,
-// 	props: {
-// 	}
-// });
-
-// export default app;
-
+import NewApp from './NewApp.svelte';
 
 const target = document.body
-new App({ target, props: { } });
+new NewApp({ target, props: { } });
